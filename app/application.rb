cb2b4b872e03bbda1fea23067b7f1d4f4add8f84
Jenise Ruehle <jela07@gmail.com>
2020-08-17 17:09:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,16 @@
 class Application
   
-  def call(env)
-    resp = Rack::Response.new 
-    resp.write Time.now.morning? ? 'Good Morning!' : ' Good Afternoon!'
-  end 
-  
-  def morning?
+   def morning?
     hour < 12
   end 
   
   def afternoon?
     hour >= 12
+  end 
+ 
+  def call(env)
+    resp = Rack::Response.new 
+    resp.write Time.now.morning? ? 'Good Morning!' : ' Good Afternoon!'
   end 
   
 end 
